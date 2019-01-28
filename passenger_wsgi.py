2d431fab0d3353ca57d30cafa3f22e6e9faacc7c
Greg Shepley <gregshepley@yahoo.co.uk>
2019-01-28 12:30:02 +0000

    updated passenger to run on python3.7

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,5 +1,5 @@
 import sys, os
-INTERP = "/home/shellgreg/.envs/lucidcooking/bin/python"
+INTERP = "/home/dh_bg554t/cooking.langato.com/env/bin/python"
 #INTERP is present twice so that the new python interpreter
 #knows the actual executable path 
 if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
@@ -8,11 +8,9 @@ cwd = os.getcwd()
 sys.path.append(cwd)
 sys.path.append(cwd + '/lucidcooking')  #You must add your project here
 
-sys.path.insert(0,'/home/shellgreg/.envs/lucidcooking/bin')
-sys.path.insert(0,'/home/shellgreg/.envs/lucidcooking/lib/python3.6/site-packages')
+sys.path.insert(0,cwd+'/env/bin')
+sys.path.insert(0,cwd+'env/lib/python3.7/site-packages')
 
 os.environ['DJANGO_SETTINGS_MODULE'] = "config.settings"
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-
-if sys.hexversion < 0x2060000: os.execl("/home/shellgreg/.envs/lucidcooking/bin/python", "python3.6", *sys.argv)
