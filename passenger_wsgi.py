3b6724d0e57643a19dbd5ede8518dc989b01d469
Greg Shepley <gregshepley@yahoo.co.uk>
2019-05-21 15:39:52 +0100

    passenger_wsgi.py updated, reverted to earlier state

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,7 +1,7 @@
 import sys
 import os
 cwd = os.getcwd()
-INTERP = cwd+"/lucidcooking/bin/python"
+INTERP = "/home/dh_bg554t/cooking.langato.com/env/bin/python"
 # INTERP is present twice so that the new python interpreter
 # knows the actual executable path
 if sys.executable != INTERP:
@@ -11,9 +11,9 @@ cwd = os.getcwd()
 sys.path.append(cwd)
 sys.path.append(cwd + '/lucidcooking')  # You must add your project here
 
-sys.path.insert(0, cwd + '/lucidcooking/env/bin')
+sys.path.insert(0, cwd + '/env/bin')
 sys.path.insert(
-    0, cwd + 'lucidcooking/env/lib/python3.7/site-packages')
+    0, cwd + '/env/lib/python3.7/site-packages')
 
 os.environ['DJANGO_SETTINGS_MODULE'] = "config.settings"
 from django.core.wsgi import get_wsgi_application
