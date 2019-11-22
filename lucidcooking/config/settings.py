5c17cb0931c6f937c286a59f310522f36e82f1d1
cooklucida <cooklucida@reliant.dreamhost.com>
2019-11-22 11:03:42 -0800

    live working

--- lucidcooking/config/settings.py
+++ lucidcooking/config/settings.py
@@ -22,15 +22,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'c%n#lh*53ffhtn^zfgb(+vhq36ekc(as(x4cpj1f2-7ec)o_r*'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['cooking.langato.com', 'www.cooking.langato.com',
                  'cooking.medialucida.co.uk', 'www.cooking.medialucida.co.uk',
-                 '127.0.0.1']
+                 '127.0.0.1',
+                 '.cooksurfing.co.uk']
 
 
 # Application definition
