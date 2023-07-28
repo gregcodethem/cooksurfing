222af85d3bb362b27c9ca41a102c99a31bb3239a
Greg Shepley <gregshepley@yahoo.co.uk>
2023-07-28 22:58:05 +0100

    passenger_wsgi.py updated for new pipenv created virtual environment on server

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,7 +1,7 @@
 import sys
 import os
 cwd = os.getcwd()
-INTERP = cwd+"/env/bin/python"
+INTERP = "/home/cooklucida/.local/share/virtualenvs/cooking.medialucida.co.uk-Rg0-2_cj"
 # INTERP is present twice so that the new python interpreter
 # knows the actual executable path
 if sys.executable != INTERP:
@@ -11,9 +11,9 @@ cwd = os.getcwd()
 sys.path.append(cwd)
 sys.path.append(cwd + '/lucidcooking')  # You must add your project here
 
-sys.path.insert(0, cwd + '/env/bin')
+sys.path.insert(0, '/home/cooklucida/.local/share/virtualenvs/cooking.medialucida.co.uk-Rg0-2_cj')
 sys.path.insert(
-    0, cwd + '/env/lib/python3.7/site-packages')
+    0, cwd + '/home/cooklucida/.local/share/virtualenvs/cooking.medialucida.co.uk-Rg0-2_cj/lib/python3.7/site-packages')
 
 os.environ['DJANGO_SETTINGS_MODULE'] = "config.settings"
 from django.core.wsgi import get_wsgi_application
